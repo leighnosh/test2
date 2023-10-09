889460330551ba1fb9a47a3a456522b9e9b51de8
leighnosh <112686904+leighnosh@users.noreply.github.com>
2023-10-09 19:47:38 +0530

    Updated dom.js
    Item 3 now has a green background.
    All the items in the list class are Bold

--- dom.js
+++ dom.js
@@ -1,16 +1,23 @@
 document.addEventListener('DOMContentLoaded', function() {
-    var headerTitle = document.getElementById('header-title');
-    var header = document.getElementById('main-header');
-    header.style.borderBottom = 'solid 8px #000'
-  
-    var addItemHeading = document.querySelector('.title');
-    addItemHeading.style.fontWeight = 'bold';
-    addItemHeading.style.color = 'green';
-    addItemHeading.style.textTransform = 'uppercase';
-    headerTitle.style.textTransform = 'uppercase';
-    headerTitle.style.color = 'black';
+  var headerTitle = document.getElementById('header-title');
+  var header = document.getElementById('main-header');
+  header.style.borderBottom = 'solid 8px #000'
 
-    var submitButton = document.querySelector('.btn-dark');
-    submitButton.style.backgroundColor = 'green';
-  });
-  
+  var addItemHeading = document.querySelector('.title');
+  addItemHeading.style.fontWeight = 'bold';
+  addItemHeading.style.color = 'green';
+  addItemHeading.style.textTransform = 'uppercase';
+  headerTitle.style.textTransform = 'uppercase';
+  headerTitle.style.color = 'black';
+
+  var submitButton = document.querySelector('.btn-dark');
+  submitButton.style.backgroundColor = 'green';
+
+  var items = document.getElementsByClassName('list-group-item');
+  items[2].style.backgroundColor = 'green';
+  for(var i = 0; i < items.length; i++){
+    items[i].style.fontWeight = 'bold';
+  }
+
+
+});
