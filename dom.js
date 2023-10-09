4c6f8eb98c5b8d40b7864dbe35ab06fa64472dca
leighnosh <112686904+leighnosh@users.noreply.github.com>
2023-10-09 21:57:31 +0530

    Changes made to dom.js using querySelector and querySelectorAll
    2nd item green background color
    3rd item invisible
    
     font color to green for 2nd item in the item list
    Choose all the odd elements and make their background green using QuerySelectorALL﻿

--- dom.js
+++ dom.js
@@ -23,5 +23,38 @@ document.addEventListener('DOMContentLoaded', function() {
   var li = document.getElementsByTagName('li');
   li[4].style.backgroundColor = 'pink'
 
+  var headerTitle = document.querySelector('#main-header')
+  header.style.border ='solid 10px pink';
+
+  var input = document.querySelector('input');
+  input.value = 'Hello World'
+
+  var submit = document.querySelector('input[type="submit"]');
+  submit.value = 'SEND';
+
+  var item = document.querySelector('.list-group-item');
+  item.style.color = 'red';
+
+  //Changing bg color for 2nd item to green
+  var secondItem = document.querySelector('.list-group-item:nth-child(2');
+  secondItem.style.backgroundColor = 'green';
+
+
+//Making  thrid item invisible
+  var thirdItem = document.querySelector('.list-group-item:nth-child(3');
+  thirdItem.style.visibility = 'hidden';
+
+
+//Making second child font green
+  var even = document.querySelectorAll('li:nth-child(even');
+  even[0].style.color = 'green'
+
+  
+
+//Making odd item background green
+  var odd = document.querySelectorAll('li:nth-child(odd');
+  for (var i = 0; i < odd.length; i++){
+    odd[i].style.backgroundColor = 'green';
+  }
 
 });
